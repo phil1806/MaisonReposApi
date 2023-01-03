18388f921e02d23758eab04e9ace76c9de2c855e
phil <tchatchoua1806@gmail.com>
2023-01-03 16:57:30 +0100

    MyFirstCommit

--- MaisonReposApi.Entities/ResidantSuivi.cs
+++ MaisonReposApi.Entities/ResidantSuivi.cs
@@ -8,7 +8,10 @@ namespace MaisonReposApi.Entities
 {
     public class ResidantSuivi
     {
+        public int Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Prenom { get; set; }
+        public string? Matricule { get; set; }
 
-        
     }
 }
